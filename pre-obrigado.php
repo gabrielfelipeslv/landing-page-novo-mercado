003331e762c776fd7086eb2daa0b0b015c6a1edd
Gabriel Felipe <gabrielfcslv@gmail.com>
2023-07-04 00:57:24 -0300

    Fix submit

--- pre-obrigado.php
+++ pre-obrigado.php
@@ -7,8 +7,10 @@ $prefixoUrl = 'https://quizenem.com/extras/projeto-o-novo-mercado';
 // Página pra alguma validação necessária ou tratamento de dados para CRM
 // Nesse caso, apenas vai checar se os dados enviados são válidos para prosseguir ou voltar atrás
 
-if(!isset($_POST)){
+if(!isset($_POST['input-nome'])){
     header("Location: {$prefixoUrl}");
 }
+
+$_SESSION['USUARIO_CADASTRADO'] = $_POST['input-nome'];
 
 header("Location: {$prefixoUrl}/obrigado");
